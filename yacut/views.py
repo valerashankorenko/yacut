d0775e0e9180e9ed193237567bbbf8bb98d9b074
Valeron <valerashankorenko@yandex.by>
2024-04-04 21:40:58 +0300

    ошибка автотестов

--- yacut/views.py
+++ yacut/views.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from flask import abort, flash, redirect, render_template
+from flask import flash, redirect, render_template
 
 from . import app, db
 from .forms import URLMapForm
